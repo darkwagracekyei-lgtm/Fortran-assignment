702bb2a02926d186f6c50e7cc92beaae71544e6f
darkwagracekyei-lgtm <darkwagracekyei@gmail.com>
2026-05-08 14:38:35 +0000

    Update Grace_6309424.f90

--- MET253_Assignment_repo/Grace_6309424.f90
+++ MET253_Assignment_repo/Grace_6309424.f90
@@ -1,31 +1,25 @@
 !6309424
-program grade
-implicit none
-real,dimension(10)::a
-integer::i
-(a)=85
-(a)=62
-(a)=45
-(a)=38
-(a)=74
-(a)=55
-(a)=88
-(a)=88
-(a)=61
-(a)=47
-do i=1,10
-read*,a
-end do
-if(i>80)and(i=80)
-print*,'Distinction'
-else if (i=60 and i<80)
-print*,'Credit'
-else if(i=40 and i<60)
-print*,'Pass'
-else if(i<40)
-print*,'Fail'
-end if
-call real,dimension(10)::a
-end program grade
+PROGRAM grade
+IMPLICIT NONE
+!Array declaration
+INTEGER,DIMENSION(10)::a=(/85,62,45,91,38,74,55,88,61,47/)
+INTEGER::i
+!loop through all students
+DO i=1,10
+!check the student's score
+IF(a(i)>=80 .AND. a(i)<=100)
+THEN
+PRINT*,'Distinction'
+Else IF(a(i)>=60 .AND. a(i)<80)
+THEN
+PRINT*,'Credit'
+Else IF(a(i)>=40 .AND. a(i)<60)
+THEN
+PRINT*,'Pass'
+Else
+PRINT*,'Fail'
+END IF
+END DO 
+END PROGRAM grade
 
 
